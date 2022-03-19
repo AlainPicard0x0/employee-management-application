2cd9975a18acb4b5ea4642203afb717a2876875a
unknown <devcoder0x0@gmail.com>
2022-03-18 20:27:38 -0700

    add style to body and .App

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      < Header />
-    </BrowserRouter>      
+    <div className="App">
+      <BrowserRouter>
+        < Header />
+      </BrowserRouter>
+
+    </div>
+          
     </>
   )
 }
