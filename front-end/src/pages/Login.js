98f8545e46aaadb5471e1fa856528654ad56f0d7
unknown <devcoder0x0@gmail.com>
2022-03-19 19:58:27 -0700

    Add login form to Login.js

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -3,6 +3,19 @@ const Login = () => {
     return (
         <div className="login">
             <h1>Login Page</h1>
+            <form className="login-form">
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input id="email" type="email" placeholder="Enter Email" required></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="password">Password</label>
+                    <input id="password" type="password" placeholder="Enter Password" required></input>
+                </div>
+                <div className="form-group">
+                    <button id="login-button">Login</button>
+                </div>
+            </form>            
         </div>
     )
 }
