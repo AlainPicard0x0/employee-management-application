2ed1b82e584054e4c5ee7c943c1d5970600e86b1
unknown <devcoder0x0@gmail.com>
2022-03-22 20:20:21 -0700

    Add JpaRepository to EmployeeRepository and add @Query

--- employee_management_api/src/main/java/com/alain/employee_management_api/repository/EmployeeRepository.java
+++ employee_management_api/src/main/java/com/alain/employee_management_api/repository/EmployeeRepository.java
@@ -1,4 +1,14 @@
 package com.alain.employee_management_api.repository;
 
-public interface EmployeeRepository {
+import com.alain.employee_management_api.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @Query("SELECT e FROM Employee e WHERE e.email = ?1")
+    Optional<Employee> findEmployeeByEmail(String email);
 }
