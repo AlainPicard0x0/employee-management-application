40230762296bdff5af1fb4f005a540a1fd553999
unknown <devcoder0x0@gmail.com>
2022-03-24 19:52:58 -0700

    Add employeeId field to login form

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -17,6 +17,8 @@ const Login = ({login, setLogin}) => {
 
     function handleSubmit(e) {
         e.preventDefault();
+        const userInputEmployeeId = document.getElementById("employeeId");
+        const employeeId = userInputEmployeeId.value;
         const userInputEmail = document.getElementById("email");
         const userInputPassword = document.getElementById("password");
         let email = userInputEmail.value;
@@ -32,6 +34,10 @@ const Login = ({login, setLogin}) => {
         <div className="login">
             <h1>Login Page</h1>
             <form className="login-form" onSubmit={ handleSubmit }>
+                <div className="form-group">
+                    <label htmlFor="employeeId">Employee Id</label>
+                    <input id="employeeId" type="number" placeholder="Enter Employee Id" required></input>
+                </div>
                 <div className="form-group">
                     <label htmlFor="email">Email</label>
                     <input id="email" type="text" placeholder="Enter Email" required></input>
