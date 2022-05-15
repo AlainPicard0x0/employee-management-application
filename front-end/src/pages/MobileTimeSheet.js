a10aa90813d6540aa54d28cf38557e389e898442
unknown <devcoder0x0@gmail.com>
2022-05-14 20:17:19 -0700

    Add MobileTimeSheet.js

---
+++ front-end/src/pages/MobileTimeSheet.js
@@ -0,0 +1,27 @@
+
+
+const MobileTimeSheet = () => {
+    return (
+        <>
+            <div className="mobile-portal">
+                <div className="mobile-header">
+                    <div className="mobile-header-selection-container">
+                        <div className="mobile-time-sheet active">Time Sheet</div>
+                        <div className="mobile-vacation"><a href="./vacation">Vacation</a></div>
+                        <div className="mobile-sick-leave">Sick Leave</div>                       
+                    </div>
+                </div>
+            </div>
+            <div className="mobile-body">
+                <div className="row-1-col-1">
+
+                </div>
+                <div className="row-2-col-1">
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MobileTimeSheet;
---
+++ front-end/src/pages/MobileVacation.js
@@ -0,0 +1,26 @@
+
+const MobileVacation = () => {
+    return (
+        <>
+            <div className="mobile-portal">
+                <div className="mobile-header">
+                    <div className="mobile-header-selection-container">
+                        <div className="mobile-time-sheet"><a href="./time-sheet">Time Sheet</a></div>
+                        <div className="mobile-vacation active">Vacation</div>
+                        <div className="mobile-sick-leave">Sick Leave</div>                       
+                    </div>
+                </div>
+            </div>
+            <div className="mobile-body">
+                <div className="row-1-col-1">
+
+                </div>
+                <div className="row-2-col-1">
+
+                </div>
+            </div>
+        </>
+    )
+}
+export default MobileVacation;
+
