764ddf1ef1e0d1a3623711cbeef7b435e418a67f
unknown <devcoder0x0@gmail.com>
2022-06-03 16:35:37 -0700

    Add mobile time-sheet page

--- front-end/src/pages/Portal.js
+++ front-end/src/pages/Portal.js
@@ -1072,12 +1072,115 @@ const Portal = ({email, login, setLogin, getEmployee, employee}) => {
             </div>
             <div className="mobile-body">
                 <div id="time-sheet-page" className="time-sheet-mobile-body mobile-container">
-                    <div className="row-1-col-1">
-                        <h2>Time Sheet Page</h2>
-                    </div>
-                    <div className="row-2-col-1">
-                        <h2>Time Sheet Page</h2>
-                    </div>
+                    <section id="mobile-row-1">
+                        <div className="mobile-vacation-greeting-section">
+                            <div className="mobile-vacation-greet-title">
+                                <h2>Welcome,</h2>
+                            </div>
+                                                                    
+                            <div className="mobile-user-icon-background">
+                                <FontAwesomeIcon icon={faUser} />
+                            </div>
+                            <div className="vacation-row-1-lower-row">
+                                <div className="oval-image-one">
+                                    <div className="mobile-person-walking-image">
+                                        <FontAwesomeIcon icon={faPersonWalkingLuggage} />
+                                    </div>
+                                    <div className="oval oval-one"></div>
+                                </div>
+                                <div className="oval-image-two">
+                                    <div className="mobile-beach-umbrella-image">
+                                        <FontAwesomeIcon icon={faUmbrellaBeach} />
+                                    </div>
+                                    <div className="oval oval-two"></div>
+                                </div>
+                                <div className="oval-image-three">
+                                    <div className="mobile-person-walking-image">
+                                        <FontAwesomeIcon icon={faPersonWalkingLuggage} />
+                                    </div>
+                                    <div className="oval oval-three"></div>
+                                </div>
+                                <div className="oval-image-four">
+                                    <div className="mobile-beach-umbrella-image">
+                                        <FontAwesomeIcon icon={faUmbrellaBeach} />
+                                    </div>
+                                    <div className="oval oval-four"></div>
+                                </div>
+                            </div>  
+                        </div>
+                    </section>
+                    <section id="vacation-row-2">
+                        <div className="mobile-vacation-leave-section">
+                            <div className="annual-leave-banner">
+                                <p>Annual Leave</p>
+                            </div>
+                            <div className="mobile-vacation-remaining-container">
+                                <div className="mobile-vacation-block mobile-vacation-remaining-block">40.0</div>
+                                <p>Vacation Remaining</p>
+                            </div>
+                            <div className="mobile-vacation-spacer-container">
+                                <div className="mobile-vacation-block"></div>
+                            </div>
+                            <div className="mobile-vacation-used-container">
+                                <div className="mobile-vacation-block mobile-vacation-used-block">16.0</div>
+                                <p>Vacation Used</p>
+                            </div>
+                            <div className="mobile-vacation-pie-container">
+                                <div className="portal-vacation-leave">
+                                    <div id="vacation-pie">
+                                        <div id="vacation-pie-background"></div>
+                                    </div>                            
+                                </div>
+                                <div className="vacation-pie-hours">
+                                    <p>40.0</p>
+                                </div>
+                            </div>
+                            <div className="mobile-vacation-request-btn-container">
+                                <button id="mobile-vacation-request-btn">Request Vacation</button>
+                            </div>
+                            
+                        </div>
+                    </section>
+                    <section id="vacation-row-three">
+                        <div className="mobile-vacation-add-remove-section">
+                            <div className="add-remove-banner">
+
+                            </div>
+                            <div className="add-remove-vacation-text">
+                                <p>Add / Remove Vacation Hours</p>
+                            </div>
+                            
+                            <div className="vac-plus-btn">
+                                <h3>Add</h3>
+                                <FontAwesomeIcon icon={faPlusCircle} />
+                            </div>
+                            <div className="vac-minus-btn">
+                                <h3>Remove</h3>
+                                <FontAwesomeIcon icon={faMinusCircle} />
+                            </div>          
+                        </div>
+                    </section>
+                    <section id="vacation-row-four">
+                        <div className="mobile-vacation-company-section">
+                            <div className="company-section-banner">
+
+                            </div>
+                            <div className="company-section-pictures">
+                                <div className="company-section-picture-1">
+                                    <img src="/images/hr-pic01.jpeg"></img>
+                                </div> 
+                                <div className="company-section-picture-2">
+                                    <img src="/images/hr-pic02.jpg"></img>
+                                </div>                               
+                            </div>
+                            <div className="company-section-boxes">
+                                <div className="left-box"></div>
+                                <div className="right-box"></div>
+                                <div className="left-box"></div>
+                                <div className="right-box"></div>
+                            </div>
+                        </div>
+                    </section>
                 </div>
                 <div id="vacation-page" className="vacation-mobile-body mobile-container selected">
                     <section id="mobile-row-1">
