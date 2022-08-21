a10f5ec653c10b8eca4d7a9bffd71edc8125addd
unknown <devcoder0x0@gmail.com>
2022-08-21 14:19:47 -0700

    Edit layout of .mobile-register-form-container

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -57,34 +57,32 @@ const Register = ({login, createEmployee}) => {
                 </div>
             </div>
             <div className="register-mobile">
-                <div className="register-form-container">
-                    <div className="register-col-left">
-                        <div className="register-form-header">
-                            <h2>Register</h2>
-                            <p>Already have an account? <a href="./">Login</a></p>
-                        </div>            
-                        <form className="register-form" onSubmit={ onSubmit }>
-                            <div className="form-group">
-                                <label htmlFor="first-name">First Name</label>
-                                <input id="first-name" type="text" placeholder="Enter First Name" required></input>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="last-name">Last Name</label>
-                                <input id="last-name" type="text" placeholder="Enter Last Name" required></input>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="email">Email</label>
-                                <input id="email" type="email" placeholder="Enter Email" required></input>
-                            </div>
-                            <div className="form-group">
-                                <label htmlFor="password">Password</label>
-                                <input id="password" type="password" placeholder="Enter Password" required></input>
-                            </div>
-                            <div className="form-group">
-                                <button id="register-button">Register</button>
-                            </div>
-                        </form>
-                    </div>
+                <div className="mobile-register-form-container">                    
+                    <div className="mobile-register-form-header">
+                        <h2>Register</h2>
+                        <p>Already have an account? <a href="./">Login</a></p>
+                    </div>            
+                    <form className="register-form" onSubmit={ onSubmit }>
+                        <div className="form-group">
+                            <label htmlFor="first-name">First Name</label>
+                            <input id="first-name" type="text" placeholder="Enter First Name" required></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="last-name">Last Name</label>
+                            <input id="last-name" type="text" placeholder="Enter Last Name" required></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input id="email" type="email" placeholder="Enter Email" required></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input id="password" type="password" placeholder="Enter Password" required></input>
+                        </div>
+                        <div className="form-group">
+                            <button id="register-button">Register</button>
+                        </div>
+                    </form>                    
                 </div>
             </div>  
         </>      
